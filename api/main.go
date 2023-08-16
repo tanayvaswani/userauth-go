171e320bcbe07b47d00a20e508d032ea442d894c
tanayvaswani <vaswani.tanay9@gmail.com>
2023-08-16 22:40:11 +0530

    Added AuthRoutes & UserRoutes

---
+++ api/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tanayvaswani/userauth-go/routes"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "7000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRoutes()
+	routes.UserRoutes()
+
+	router.GET("/api-1", func (c *gin.Context) {
+		c.JSON(200, gin.H{"Success":"Access granted for api-1"})
+	})
+
+	router.GET("/api-2", func (c *gin.Context) {
+		c.JSON(200, gin.H{"Success":"Access granted for api-2"})
+	})
+
+	router.Run(":" + port)
+}
---
+++ api/routes/userRouter.go
@@ -0,0 +1,7 @@
+package routes
+
+import ()
+
+func UserRoutes() {
+	
+}
